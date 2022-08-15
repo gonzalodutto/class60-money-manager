f4272455389896efa0145f5d9d08723a3117d5f1
gonzalodutto <gonzalodutto@gmail.com>
2022-08-15 16:09:10 +0200

    css changes

--- src/App.js
+++ src/App.js
@@ -3,53 +3,62 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectBalance } from "./store/balance/selectors";
 import { useState } from "react";
 
+import "./App.css";
+
 function App() {
   const dispatch = useDispatch();
-  const balance = useSelector(selectBalance);
+  // const balance = useSelector(selectBalance);
+  const balance = useSelector((reduxState) => reduxState.balance.amount);
   const [customAmount, setCustomAmount] = useState(0);
 
   return (
     <div className="App">
-      <p>Balance: {balance}$</p>
-      <button
-        onClick={() => {
-          dispatch(deposit(10));
-        }}
-      >
-        Deposit 10$
-      </button>
-      <button
-        onClick={() => {
-          dispatch(withdraw(7));
-        }}
-      >
-        Withdraw 7$
-      </button>
-      <button
-        onClick={() => {
-          dispatch(reset(7));
-        }}
-      >
-        Reset
-      </button>
-      <br />
-      <br />
-      <input
-        type="number"
-        value={customAmount}
-        onChange={(e) => {
-          setCustomAmount(parseInt(e.target.value));
-        }}
-      />
-      <br />
-      <button
-        onClick={() => {
-          dispatch(deposit(customAmount));
-          setCustomAmount(0);
-        }}
-      >
-        Deposit custom amount
-      </button>
+      <h1>Balance: {balance}$</h1>
+
+      <div>
+        <button
+          onClick={() => {
+            dispatch(deposit(10));
+          }}
+        >
+          Deposit 10$
+        </button>
+        <p>
+          <button
+            onClick={() => {
+              dispatch(withdraw(7));
+            }}
+          >
+            Withdraw 7$
+          </button>
+        </p>
+        <button
+          onClick={() => {
+            dispatch(reset(7));
+          }}
+        >
+          Reset
+        </button>
+      </div>
+      <div>
+        <p>
+          <input
+            type="number"
+            value={customAmount}
+            onChange={(e) => {
+              setCustomAmount(parseInt(e.target.value));
+            }}
+          />
+        </p>
+        <button
+          onClick={() => {
+            dispatch(deposit(customAmount));
+            setCustomAmount(0);
+          }}
+        >
+          Deposit custom amount
+        </button>
+      </div>
     </div>
   );
 }
