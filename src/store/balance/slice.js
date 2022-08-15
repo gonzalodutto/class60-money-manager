078f9e49ed45c767558cfa50e5f95f8756c978ba
gonzalodutto <gonzalodutto@gmail.com>
2022-08-15 14:12:40 +0200

    Dispatch Action: Sending data to the store

--- src/store/balance/slice.js
+++ src/store/balance/slice.js
@@ -7,11 +7,16 @@ const initialState = {
 export const balanceSlice = createSlice({
   name: "balance",
   initialState,
-  reducers: {},
+  reducers: {
+    // Add a key called `deposit`
+    deposit: (state) => {
+      // empty for now! :)
+    },
+  },
 });
 
 // Action creators are generated for each case reducer function
 // as we add cases to our reducer we will also export the corresponding actions
-export const {} = balanceSlice.actions;
+export const { deposit } = balanceSlice.actions;
 
 export default balanceSlice.reducer;
